f0c4ddfb48e8212cf0db259f5ca3b7c4987b3e72
Rosenberg Ruty <rosenbergruty@gmail.com>
2023-06-26 23:18:07 +0300

    commit2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <exception>
 
-#include "CitySearch.cpp"
+#include "CitySearch.h"
 using namespace std;
 int main() 
 {
@@ -22,17 +22,54 @@ int main()
 
         if (cityName == "0") {
             break;
-        } 
+        }
+       
+
+        // Check if the city exists
+        if (citySearch.isCity(cityName)) {
+            std::cout << "ERROR: \"" << cityName << "\" isn't found in the city list. Please try again." << std::endl;
+            continue;
+        }
 
         std::cout << "Please enter the wanted radius:" << std::endl;
         double radius;
-        std::cin >> radius;
+        try {
+            if (!(std::cin >> radius)) {
+                throw std::runtime_error("Invalid radius: ");
+            }
+            while  (radius <= 0) {
+                std::cout << "ERROR: Radius cannot be negative. Please enter a non-negative value." << std::endl;
+                if(!(std::cin >> radius)) {
+                    throw std::runtime_error("Invalid radius: ");
+                }
+            }
+        }
+        catch (const std::exception& e) {
+            std::cout << "ERROR: " << e.what() << std::endl;
+            return 1;  // Terminate the program
+        }
 
         std::cout << "Please enter the wanted norm: \n 0 = L2, Euclidean distance,\n 1 = Linf, Chebyshev distance,\n 2 = L1, Manhattan distance):\n" << std::endl;
         int norm;
-        std::cin >> norm;
+        try {
+            if (!(std::cin >> norm)) {
+                throw std::runtime_error("Invalid norm: ");
+            }
+            while (norm != 0 and norm != 1 and norm != 2)
+            {
+                std::cout << "ERROR: Invalid norm value. Please enter a value of 0, 1, or 2." << std::endl;
+                if (!(std::cin >> norm)) {
+                    throw std::runtime_error("Invalid norm: ");
+                }
+            }
 
-        //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        catch (const std::exception& e) {
+            std::cout << "ERROR: " << e.what() << std::endl;
+            return 1;  // Terminate the program
+        }
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         std::cout << "Search result:" << std::endl;
         citySearch.searchCities(cityName, radius, norm);
